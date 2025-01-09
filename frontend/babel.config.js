dd9179b838ed144705be55133c3b7083201df647
NiyonAlex <alexis.niyonkuru1984@gmail.com>
2025-01-09 15:55:02 -0500

    frontend Setup

---
+++ frontend/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", // Transforms modern JavaScript
+    "@babel/preset-react", // Transforms JSX
+  ],
+  plugins: ["@babel/plugin-transform-modules-commonjs"],
+};
